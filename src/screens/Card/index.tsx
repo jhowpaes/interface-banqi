c382e79e0db73aa3fc5dfbf3d7a131f3cf8f9afe
jhowpaes <jhowylli@gmail.com>
2021-03-10 11:16:19 -0300

    ✨ add card page

---
+++ src/screens/Card/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { Container } from './styles';
+
+const Card: React.FC = () => {
+  return <Container>
+    <Text>Card</Text>
+  </Container>;
+}
+
+export default Card;
