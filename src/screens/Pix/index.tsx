5578a2678f2ff18f2d41a5facaf38f52eef56ec3
jhowpaes <jhowylli@gmail.com>
2021-03-10 11:17:01 -0300

    ✨ add pix page

---
+++ src/screens/Pix/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { Container } from './styles';
+
+const Pix: React.FC = () => {
+  return <Container>
+    <Text>Pix</Text>
+  </Container>;
+}
+
+export default Pix;
