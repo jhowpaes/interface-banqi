853548bacaedc9c5cd2ab98fa045f79837fe4774
jhowpaes <jhowylli@gmail.com>
2021-03-10 11:17:20 -0300

    ✨ add purchasing page

---
+++ src/screens/Purchasing/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { Container } from './styles';
+
+const Purchasing: React.FC = () => {
+  return <Container>
+    <Text>Purchasing</Text>
+  </Container>;
+}
+
+export default Purchasing;
