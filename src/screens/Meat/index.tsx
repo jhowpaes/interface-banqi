c5e2689dc48ba503bcb47dfe3276530147612321
jhowpaes <jhowylli@gmail.com>
2021-03-10 11:16:44 -0300

    ✨ add meat page

---
+++ src/screens/Meat/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { Container } from './styles';
+
+const Meat: React.FC = () => {
+  return <Container>
+    <Text>Meat</Text>
+  </Container>;
+}
+
+export default Meat;
